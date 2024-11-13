1601aacf0372987515f949b8aa16bc3d5dd0f7f6
Peggy <peggy.chuang@neualgo.co>
2024-11-13 18:46:28 +0800

    refactor: extract static data to separated file

--- app/(main)/preferance-flow/page.tsx
+++ app/(main)/preferance-flow/page.tsx
@@ -27,6 +27,8 @@ import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
 // import Link from "next/link";
 
+import { placeOptions, sportTypeOptions, purposeOptions, frequencyOptions } from "@/consts/preferance-flow-options";
+
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
@@ -56,60 +58,60 @@ const formSchema = z
   })
   .required();
 
-// place options
-const placeOptions = [
-  { value: "gym", text: "健身房" },
-  { value: "home", text: "家裡" },
-  { value: "park", text: "公園或戶外" },
-  { value: "community_gym", text: "社區運動場（運動中心）" },
-  { value: "other", text: "其他" },
-];
+// // place options
+// const placeOptions = [
+//   { value: "gym", text: "健身房" },
+//   { value: "home", text: "家裡" },
+//   { value: "park", text: "公園或戶外" },
+//   { value: "community_gym", text: "社區運動場（運動中心）" },
+//   { value: "other", text: "其他" },
+// ];
 
-const sportTypeOptions = [
-  { value: "running", text: "跑步" },
-  { value: "hiking", text: "爬山/健行" },
-  { value: "yoga", text: "瑜珈" },
-  { value: "swimming", text: "游泳" },
-  { value: "ball_games", text: "球類運動" },
-  { value: "climbing", text: "攀岩/抱石" },
-  { value: "retraining", text: "重訓" },
-  { value: "bicycling", text: "自行車" },
-  { value: "other", text: "其他" },
-];
+// const sportTypeOptions = [
+//   { value: "running", text: "跑步" },
+//   { value: "hiking", text: "爬山/健行" },
+//   { value: "yoga", text: "瑜珈" },
+//   { value: "swimming", text: "游泳" },
+//   { value: "ball_games", text: "球類運動" },
+//   { value: "climbing", text: "攀岩/抱石" },
+//   { value: "retraining", text: "重訓" },
+//   { value: "bicycling", text: "自行車" },
+//   { value: "other", text: "其他" },
+// ];
 
-const frequencyOptions = [
-  { value: "daily", text: "每天" },
-  { value: "4_5_a_week", text: "每週4~5次" },
-  { value: "2_3_a_week", text: "每週2~3次" },
-  { value: "once_a_week", text: "每週1次" },
-  { value: "rarely", text: "幾乎不運動" },
-];
+// const frequencyOptions = [
+//   { value: "daily", text: "每天" },
+//   { value: "4_5_a_week", text: "每週4~5次" },
+//   { value: "2_3_a_week", text: "每週2~3次" },
+//   { value: "once_a_week", text: "每週1次" },
+//   { value: "rarely", text: "幾乎不運動" },
+// ];
 
-const purposeOptions = [
-  {
-    value: "for_health",
-    text: "健康維護（增加體力、提高免疫力、保持身體健康）",
-  },
-  { value: "lose_weight", text: "體重控制（減肥、減少體脂，或維持理想體重）" },
-  { value: "relax", text: "壓力舒緩（減少壓力、緊張和焦慮，改善心理健康）" },
-  {
-    value: "improve_muscle",
-    text: "增強肌肉和力量（增加肌肉質量、提高力量和耐力）",
-  },
-  {
-    value: "improve_cardio",
-    text: "提升心肺功能（改善心肺健康，增強體能和耐力）",
-  },
-  {
-    value: "improve_body_shape",
-    text: "改善體態（增強身體線條，塑造理想身形）",
-  },
-  {
-    value: "for_social",
-    text: "社交活動（結交朋友、增加人際互動，參與團體活動）",
-  },
-  { value: "other", text: "其他個人目標" },
-];
+// const purposeOptions = [
+//   {
+//     value: "for_health",
+//     text: "健康維護（增加體力、提高免疫力、保持身體健康）",
+//   },
+//   { value: "lose_weight", text: "體重控制（減肥、減少體脂，或維持理想體重）" },
+//   { value: "relax", text: "壓力舒緩（減少壓力、緊張和焦慮，改善心理健康）" },
+//   {
+//     value: "improve_muscle",
+//     text: "增強肌肉和力量（增加肌肉質量、提高力量和耐力）",
+//   },
+//   {
+//     value: "improve_cardio",
+//     text: "提升心肺功能（改善心肺健康，增強體能和耐力）",
+//   },
+//   {
+//     value: "improve_body_shape",
+//     text: "改善體態（增強身體線條，塑造理想身形）",
+//   },
+//   {
+//     value: "for_social",
+//     text: "社交活動（結交朋友、增加人際互動，參與團體活動）",
+//   },
+//   { value: "other", text: "其他個人目標" },
+// ];
 
 const PreferanceFlow = () => {
   const [currentStep, setCurrentStep] = useState(1);
@@ -133,17 +135,17 @@ const PreferanceFlow = () => {
     },
   });
 
-  console.log(form);
-
   const onSubmit = (data: z.infer<typeof formSchema>) => {
     // sent to backend
     console.log(data);
   };
 
-  const handlePrevStep = () => {
+  const handlePrevStep = (e: React.FormEvent) => {
+    e.preventDefault();
     setCurrentStep(currentStep - 1);
   };
-  const handleNextStep = () => {
+  const handleNextStep = (e: React.FormEvent) => {
+    e.preventDefault();
     if (currentStep < 6) {
       setCurrentStep(currentStep + 1);
     } else {
