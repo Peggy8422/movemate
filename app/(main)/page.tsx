6e09d8713e3eb55a63ebd740064e7ca91dacfd34
Peggy <peggy.chuang@neualgo.co>
2024-11-14 18:38:47 +0800

    feat: add next-auth and test line login OAuth

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,5 +1,16 @@
+"use client";
+
+import React from "react";
+import { useSearchParams } from "next/navigation";
+
 
 export default function Home() {
+  const searchParams = useSearchParams();
+  // if use Line login
+  const code = searchParams.get("code");
+
+  console.log(code);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
--- app/(main)/preferance-flow/page.tsx
+++ app/(main)/preferance-flow/page.tsx
@@ -58,61 +58,6 @@ const formSchema = z
   })
   .required();
 
-// // place options
-// const placeOptions = [
-//   { value: "gym", text: "健身房" },
-//   { value: "home", text: "家裡" },
-//   { value: "park", text: "公園或戶外" },
-//   { value: "community_gym", text: "社區運動場（運動中心）" },
-//   { value: "other", text: "其他" },
-// ];
-
-// const sportTypeOptions = [
-//   { value: "running", text: "跑步" },
-//   { value: "hiking", text: "爬山/健行" },
-//   { value: "yoga", text: "瑜珈" },
-//   { value: "swimming", text: "游泳" },
-//   { value: "ball_games", text: "球類運動" },
-//   { value: "climbing", text: "攀岩/抱石" },
-//   { value: "retraining", text: "重訓" },
-//   { value: "bicycling", text: "自行車" },
-//   { value: "other", text: "其他" },
-// ];
-
-// const frequencyOptions = [
-//   { value: "daily", text: "每天" },
-//   { value: "4_5_a_week", text: "每週4~5次" },
-//   { value: "2_3_a_week", text: "每週2~3次" },
-//   { value: "once_a_week", text: "每週1次" },
-//   { value: "rarely", text: "幾乎不運動" },
-// ];
-
-// const purposeOptions = [
-//   {
-//     value: "for_health",
-//     text: "健康維護（增加體力、提高免疫力、保持身體健康）",
-//   },
-//   { value: "lose_weight", text: "體重控制（減肥、減少體脂，或維持理想體重）" },
-//   { value: "relax", text: "壓力舒緩（減少壓力、緊張和焦慮，改善心理健康）" },
-//   {
-//     value: "improve_muscle",
-//     text: "增強肌肉和力量（增加肌肉質量、提高力量和耐力）",
-//   },
-//   {
-//     value: "improve_cardio",
-//     text: "提升心肺功能（改善心肺健康，增強體能和耐力）",
-//   },
-//   {
-//     value: "improve_body_shape",
-//     text: "改善體態（增強身體線條，塑造理想身形）",
-//   },
-//   {
-//     value: "for_social",
-//     text: "社交活動（結交朋友、增加人際互動，參與團體活動）",
-//   },
-//   { value: "other", text: "其他個人目標" },
-// ];
-
 const PreferanceFlow = () => {
   const [currentStep, setCurrentStep] = useState(1);
 
