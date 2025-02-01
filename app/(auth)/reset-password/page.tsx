e58314d81baddda79884d64a80ec09d1374c497e
Peggy <peggy.chuang@neualgo.co>
2025-02-01 17:08:32 +0800

    feat: add reset-password page

---
+++ app/(auth)/reset-password/page.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import React from "react";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+const formSchema = z
+  .object({
+    account: z.string().email(),
+    password: z.string().min(6).max(12),
+    confirmPassword: z.string().min(6).max(12),
+  })
+  .required();
+
+const ResetPasswordForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      account: "",
+      password: "",
+      confirmPassword: "",                                          
+    },               
+  });
+
+  const onSubmit = (data: z.infer<typeof formSchema>) => {
+    // sent to backend
+    console.log(data);
+  };
+
+  return (
+    <Form {...form}>
+      <h1 className="text-3xl font-bold text-primary text-center mb-3">
+        重設密碼
+      </h1>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="account"
+          render={({ field }) => (
+            <FormItem className="mb-3">
+              <FormLabel>帳戶（電子信箱）</FormLabel>
+              <FormControl>
+                <Input
+                  type="email"
+                  placeholder="輸入註冊時的電子信箱（ex: test@example.com)"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem className="mb-3">
+              <FormLabel>設定新密碼</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="請設定6~12位英文字母大小寫和數字混合的密碼"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="confirmPassword"
+          render={({ field }) => (
+            <FormItem className="mb-3">
+              <FormLabel>再次確認新密碼</FormLabel>
+              <FormControl>
+                <Input
+                  type="password"
+                  placeholder="請再次輸入密碼"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <div className="flex items-center justify-center gap-2 mt-10">
+          <Button type="submit" size="sm" className="w-1/4 min-w-[150px]">
+            確認
+          </Button>
+        </div>
+      </form>
+    </Form>
+  );
+};
+
+const ResetPassword = () => {
+  return (
+    <div className="h-full position-relative">
+      <ResetPasswordForm />
+    </div>
+  );
+};
+
+export default ResetPassword;
