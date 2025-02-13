b8292b516db5b67d76a2a577d00e9e7d45bb0335
Peggy8422 <pig2222pig222@gmail.com>
2025-02-14 00:04:32 +0800

    fix: remove unused code for deployment

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import React from "react";
-import { useRouter, usePathname } from "next/navigation";
+import { usePathname } from "next/navigation";
 import {
   Sidebar,
   SidebarContent,
