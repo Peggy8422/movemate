ac44449c91ed15fae97334a636dd79f67c502b96
Peggy8422 <pig2222pig222@gmail.com>
2025-02-18 01:26:35 +0800

    fix: test middleware matcher remove for prod

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,22 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
+  // console.log("Middleware Path:", request.nextUrl.pathname); // 記錄請求路徑
+  const { pathname } = request.nextUrl;
+  if (
+    pathname.startsWith("/api") ||
+    pathname.startsWith("/_next") ||
+    pathname === "/favicon.ico" ||
+    pathname === "/sitemap.xml" ||
+    pathname === "/robots.txt" ||
+    pathname.startsWith("/sign-in") ||
+    pathname.startsWith("/sign-up") ||
+    pathname.startsWith("/login-redirect") ||
+    pathname.startsWith("/reset-password")
+  ) {
+    return NextResponse.next();
+  }
+
   if (!request.cookies.has("token")) {
     return NextResponse.redirect(new URL("/sign-in", request.url));
   } else {
@@ -9,8 +25,8 @@ export function middleware(request: NextRequest) {
   }
 }
 
-export const config = {
-  matcher: [
-    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|sign-in|sign-up|login-redirect|reset-password).*)",
-  ],
-};
+// export const config = {
+//   matcher: [
+//     "/((?!api/|_next/|favicon.ico|sitemap.xml|robots.txt|sign-in|sign-up|login-redirect|reset-password).*)",
+//   ],
+// };
